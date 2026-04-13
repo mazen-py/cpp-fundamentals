56df6b8438b8f9155bcf780f783e7ccbe4194335
mazen-py <mazenkue@gmail.com>
2026-04-13 23:19:53 +0200

    Add countdown using a for loop
    Implemented a definite 'for' loop that iterates through a predefined list of integers to create a countdown effect.

---
+++ PY/countdown_for_loop.py
@@ -0,0 +1,9 @@
+"""
+Program Name: Countdown For Loop
+Description: Demonstrates iterating through a fixed list of numbers using a for loop.
+Concepts used: Lists and For Loops.
+"""
+for i in [5, 4, 3, 2, 1]:
+    print(i)
+    
+print("Okay, countdown finished!")
